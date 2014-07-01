7e78b3a081f99e6947d8ef933a483b86ebe5b40c
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-07-01 16:53:51 -0700

    Completed even Fibonacci numbers challenge

---
+++ euler_algorithms.rb
@@ -0,0 +1,18 @@
+def fib_sequence(limit)
+  base = [1, 2]
+  while (base.last < limit)
+    next_num = base[-1] + base[-2]
+    base << next_num
+  end
+  base.pop
+  base
+end
+
+def get_sum_evens(sequence)
+  evens = sequence.keep_if {|num| num % 2 == 0 }
+  evens.inject(:+)
+end
+
+
+# Driver Code
+p (get_sum_evens(fib_sequence(4000000)) == 4613732) #=> true
