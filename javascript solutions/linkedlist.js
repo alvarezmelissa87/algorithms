1a182bc09a91f04ed439beec7230324cf43d5bf4
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-07-12 15:49:06 -0700

    Created remove function to LinkedList prototype- can now remove item from linked list

--- javascript solutions/linkedlist.js
+++ javascript solutions/linkedlist.js
@@ -20,3 +20,23 @@ LinkedList.prototype.add = function(value) {
   }
   return node;
 };
+
+LinkedList.prototype.remove = function(node) {
+  var current, value = node.value;
+
+  if(this.head != null) {
+    if(this.head === node) {
+      this.head = this.head.next;
+      node.next = null;
+      return value;
+    }
+    current = this.head;
+    while(current.next) {
+      if(current.next === node) {
+        current.next = node.next;
+        return value;
+      }
+      current = current.next;
+    }
+  }
+};
