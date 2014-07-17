caf2505c21d57fedb7950ac9a37fb5080474d186
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-07-16 18:31:45 -0700

    created method to generate multiples of target number

---
+++ ruby solutions/smallest_multiple.rb
@@ -0,0 +1,14 @@
+
+def generate_multiples(number)
+  multiples = []
+  counter = 0
+  until counter == 1000
+    multiples << number * counter
+  end
+  multiples
+end
+
+
+def smallest_multiple(limit)
+
+end
