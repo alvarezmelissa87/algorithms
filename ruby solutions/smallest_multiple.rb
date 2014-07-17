663b9019922402aca17225b7bfcce6640956e00f
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-07-16 18:41:32 -0700

    added method to generate factors

--- ruby solutions/smallest_multiple.rb
+++ ruby solutions/smallest_multiple.rb
@@ -8,7 +8,25 @@ def generate_multiples(number)
   multiples
 end
 
+def generate_factors(limit)
+  factors = []
+  until limit == 1 do
+    factors << limit
+    limit -= 1
+  end
+  factors
+end
 
 def smallest_multiple(limit)
+  multiples = generate_multiples(limit)
+  factors = generate_factors(limit)
 
+  multiples.each do |multiple|
+    number = limit
+    until number == 1 do
+      if multiple % limit == 0
+        number -=1
+      end
+    end
+  end
 end
