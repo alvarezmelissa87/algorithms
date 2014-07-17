b555bd597e78ae7e78df51555161a32b1984e0d9
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-07-17 16:30:27 -0700

    trying different approach to solving the problem

--- ruby solutions/smallest_multiple.rb
+++ ruby solutions/smallest_multiple.rb
@@ -1,3 +1,22 @@
+require 'prime'
+
+def prime_nums(number)
+  primes = [number]
+  Prime.each(10000) do |prime|
+    if (primes.last % prime == 0)
+      n = primes.pop/prime
+      primes << prime
+      primes << n
+    else
+      primes
+    end
+  end
+  primes
+end
+
+# Get the prime numbers from each of the numbers starting with the limit and
+# decrementing down by one. Pull out the prime factors used most and multiply
+# together to get smallest multiple.
 
 def generate_multiples(number)
   multiples = []
@@ -18,15 +37,10 @@ def generate_factors(limit)
 end
 
 def smallest_multiple(limit)
-  multiples = generate_multiples(limit)
+  # multiples = generate_multiples(limit)
   factors = generate_factors(limit)
-
-  multiples.each do |multiple|
-    number = limit
-    until number == 1 do
-      if multiple % limit == 0
-        number -=1
-      end
-    end
+  factors.map do |factor|
+    prime_nums(factor)
   end
+
 end
