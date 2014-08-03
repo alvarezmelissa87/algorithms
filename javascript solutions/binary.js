6ae47e93ff82978b7f018b2ee81f1c79ea5bf147
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-08-02 21:52:25 -0700

    Created logic for binary search to occur and index of searched item will be returned or -1 if not found in list

--- javascript solutions/binary.js
+++ javascript solutions/binary.js
@@ -8,12 +8,22 @@ function binarySearch(list, item) {
   var len = list.length;
   var start = 0;
   var end = len - 1;
+  var mid;
 
   while(start <= end) {
+    mid = (start + end) / 2;
 
+    if (sorted[mid] < item) {
+      start = mid + 1;
+    } else if (sorted[mid] > item){
+      end = mid - 1;
+    } else {
+      return mid;
+    }
   }
+  return -1;
 }
 
 var myNums = [30, 2, 1, 9, 15];
-myNums.sort(sortLowHigh);
-console.log(myNums);
+console.log(binarySearch(myNums, 2));
+
