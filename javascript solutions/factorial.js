a0a9684fe592cc2a3c9a3b889b9e198364734262
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-08-08 11:35:29 -0700

    Completed working factorial function

--- javascript solutions/factorial.js
+++ javascript solutions/factorial.js
@@ -1,7 +1,10 @@
 function factorial(number) {
   var result = 1;
   for (var i = number; i >= 1; i--) {
-    result = i *= number;
+    result *= i;
   }
   return result;
 }
+
+factorial(10)
+factorial(2)
