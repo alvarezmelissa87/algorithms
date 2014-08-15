9b648567e2cec8f67d0f0fa7b2cf3cbf05f6456f
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-08-15 16:58:21 -0700

    completed fibonacci sequence algorithm in javascript

--- javascript solutions/fibonacci.js
+++ javascript solutions/fibonacci.js
@@ -2,26 +2,37 @@
 
 var fibSequence = function(limit) {
   var base = [1, 2];
-  while (base[base.length - 1] < limit) {
-    var next = base[(-1)] + base[(-2)];
+  while (base.length < limit) {
+    var next = base[base.length - 1] + base[base.length - 2];
     base.push(next);
   }
-  base.pop();
+
+  console.log(base);
   return base;
 }
 
 var getSumEvens = function(sequence) {
   var result = [];
-  var i;
-  for (i = 0; i < sequence.length; ++i) {
+  var sum = 0;
+
+  for (var i = 0; i < sequence.length; i++) {
     if (sequence[i] % 2 === 0) {
       result.push(sequence[i]);
     }
   }
 
-  result = result.reduce(function(previousValue, currentValue, index, array){
-    return previousValue + currentValue;
-  });
-
-  return result;
+  for (var j = 0; j < result.length; j++) {
+    sum += result[j];
+  }
+  console.log(result);
+  console.log(sum);
+  return sum
 }
+
+
+var mySequence = fibSequence(10);
+getSumEvens(mySequence);
+
+var otherSeq = fibSequence(50);
+getSumEvens(otherSeq);
+
