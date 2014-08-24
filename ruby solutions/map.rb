f20117164981ee7101a0a415f07f29fda87d6e6f
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-08-24 13:25:18 -0700

    Implemented map with lambda

--- ruby solutions/map.rb
+++ ruby solutions/map.rb
@@ -27,3 +27,46 @@ class Array
     end
   end
 end
+
+
+# OR (to see more clearly what a Proc is)
+class Array
+  def map!(code)
+    self.each_with_index do |n, i|
+      self[i] = code.call(n)
+    end
+  end
+end
+
+first_list = [1, 2, 3, 4]
+second_list = [2, 3, 4, 5]
+
+square = Proc.new {|n| n ** 2}
+
+first_list.map!(sqare)
+second_list.map!(square)
+
+p first_list  # => [1, 4, 9, 16]
+p second_list # => [4, 9, 16, 25]
+
+# Now implemented with lambda
+
+class Array
+  def map!(code)
+    self.each_with_index do |n, i|
+      self[i] = code.call(n)
+    end
+  end
+end
+
+list = [1, 2, 3, 4]
+list.map!(lambda {|n| n ** 2})
+p list #=> [1, 4, 9, 16]
+
+
+
+
+
+
+
+
