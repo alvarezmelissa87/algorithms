08ed5d4c3711548c7659012f66b01aec6af03f35
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-08-24 17:25:18 -0700

    Added functionality for case when there is no mode

--- ruby solutions/mode.rb
+++ ruby solutions/mode.rb
@@ -1,10 +1,18 @@
 def mode(list)
   frequency = {}
   list.each do |num|
-    frequency.has_key? ? frequency[num] += 1 : frequency[num] = 1
+    frequency.has_key?(num) ? frequency[num] += 1 : frequency[num] = 1
   end
-  p frequency
-  p frequency.values.max
+
+  frequency.each do |k,v|
+    frequency[k] == frequency.values.max ? puts k : "no mode, sucka"
+  end
+
 end
 
 numbers = [1, 2, 2, 3, 3, 3, 3]
+list = [4, 4, 6, 6, 2, 2, 2, 2, 2, 1]
+more_nums = [1, 1, 2, 2, 3, 3]
+mode(numbers) #=> 3
+mode(list) #=> 2
+mode(more_nums) #=> "no mode, sucka"
