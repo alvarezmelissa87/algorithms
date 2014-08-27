bddb3eb4d22dd9a7d28ecaae13be60ea907a15f7
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-08-27 15:25:34 -0700

    completed ruby implementation of quicksort

--- ruby solutions/quick_sort.rb
+++ ruby solutions/quick_sort.rb
@@ -1,49 +1,3 @@
-# def quicksort(array, from=0, to=nil)
-#     if to == nil             # Sort the whole array, by default
-#         to = array.size - 1
-#     end
-
-#     if from >= to            # Done sorting
-#         return
-#     end
-
-#     # Take a pivot value, at the far left
-#     pivot = array[from]
-
-#     # Min and Max pointers
-#     min = from
-#     max = to
-
-#     # Current free slot
-#     free = min
-
-#     while min < max
-#         if free == min # Evaluate array[max]
-#             if array[max] <= pivot # Smaller than pivot, must move
-#                 array[free] = array[max]
-#                 min += 1
-#                 free = max
-#             else
-#                 max -= 1
-#             end
-#         elsif free == max # Evaluate array[min]
-#             if array[min] >= pivot # Bigger than pivot, must move
-#                 array[free] = array[min]
-#                 max -= 1
-#                 free = min
-#             else
-#                 min += 1
-#             end
-#         else
-#             raise "Inconsistent state"
-#         end
-#     end
-
-#     array[free] = pivot
-
-#     quicksort array, from, free - 1
-#     quicksort array, free + 1, to
-# end
 
  def quicksort a
   (pivot = a.pop) ? quicksort(a.select{|i| i <= pivot}) + [pivot] + quicksort(a.select{|i| i > pivot}) : []
@@ -52,6 +6,7 @@
  def qsort(list)
   return [] if list.size == 0
   x, *xs = *list
+  p x
   less, more = xs.partition{|y| y < x}
   qsort(less) + [x] + qsort(more)
  end
