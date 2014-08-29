fbd754994cc8e28e8122f69dde3cd8a7397862c0
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-08-29 00:15:50 -0700

    Created logic for replacing numbers with proper letters

--- ruby solutions/fizzbuzz.rb
+++ ruby solutions/fizzbuzz.rb
@@ -1,5 +1,19 @@
 my_array = IO.readlines(ARGV[0])
 
 def fizzbuzz(a, b, n)
+  a = a.to_i
+  b = b.to_i
+  n = n.to_i
 
+   for i in (1..n).to_a do
+    if i % (a * b) == 0
+      print "FB\s"
+    elsif i % b == 0
+      print "B\s"
+    elsif i % a == 0
+      print "A\s"
+    else
+      print i.to_s + "\s"
+    end
+  end
 end
