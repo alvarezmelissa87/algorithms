e6a3207340b2ebb36892d857d609507fbbd0f6c9
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-09-01 21:47:29 -0700

    Inorder traversal now possible :

--- ruby solutions/binary_tree.rb
+++ ruby solutions/binary_tree.rb
@@ -44,4 +44,39 @@ class BinarySearchTree
     puts node.value.to_S
   end
 
+  # Post-order[edit]
+  #   Traverse the left subtree.
+  #   Traverse the right subtree
+  #   Visit the root
+  def postorder_traversal(node=@root)
+    return if (node == nil)
+    puts node.value.to_s
+    postorder_traversal(node.left)
+    postorder_traversal(node.right)
+  end
+
+  #In-order traversal: display final out put in sorted order
+  #display smaller children first (by going left)
+  #then display the value in the current node (smaller than right but bigger than left)
+  #then display the larger children on the right
+  def inorder_traversal(node=@root)
+    return if (node==nil)
+    inorder_traversal(node.left)
+    puts node.value.to_s
+    inorder_traversal(node.right)
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
