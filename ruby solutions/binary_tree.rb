07e8748a0e783288606834badef8e6eeb2bcff38
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-09-01 21:19:38 -0700

    Added insert functionality to Binary tree

--- ruby solutions/binary_tree.rb
+++ ruby solutions/binary_tree.rb
@@ -7,3 +7,28 @@ class TreeNode
     @right = right
   end
 end
+
+class BinarySearchTree
+  def initialize(val)
+    puts "Initializing with: " + val.to_S
+    @root = TreeNode.new(val, nil, nil)
+  end
+
+  def insert(val)
+    puts "Inserting :" + value.to_S
+    current_node = @root
+    while current_node != nil
+      if value < current_node.value && current_node.left == nil
+        current_node.left = TreeNode.new(value, nil, nil)
+      elsif value > current_node.value && current_node.right == nil
+        current_node.right = TreeNode.new(value, nil, nil)
+      elsif value < current_node.value
+        current_node = current_node.left
+      elsif value > current_node.value
+        current_node = current_node.right
+      else
+        return
+      end
+    end
+  end
+end
